23cc463074c9d0f69f4b9ebf1be553caba9745d1
meiyushuku <78764898+meiyushuku@users.noreply.github.com>
2021-03-01 08:59:29 +0800

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+
+$json_file = file_get_contents('/volume1/confidential/confidential.json');
+$json_data = json_decode($json_file, true);
+
+$host = $json_data['host'];
+$port = $json_data['port'];
+$dbname = $json_data['dbname'];
+$dbn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+$user = $json_data['user'];
+$password = $json_data['password'];
+
+try {
+    $connect = new PDO($dbn, $user, $password);
+    //echo "Successfully!\n";
+} catch (PDOException $e) {
+    echo $e->getMessage() . "\n";
+    exit();
+}
